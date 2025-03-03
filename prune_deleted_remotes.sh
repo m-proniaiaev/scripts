2959f1f62ca6fecc6695bb2eed17d8823fd9d057
Proniaiev Maksym <69893242+m-proniaiaev@users.noreply.github.com>
2025-03-03 12:45:56 +0200

    Update prune_deleted_remotes.sh

--- prune_deleted_remotes.sh
+++ prune_deleted_remotes.sh
@@ -1,1 +1,3 @@
 git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs -r git branch -d -f
+
+git config --global alias.cleanup '!git branch -vv | grep ": gone]" | grep -v "\*" | awk "{ print \$1; }" | xargs -r git branch -d -f'
