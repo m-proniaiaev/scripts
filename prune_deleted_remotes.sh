b07ff6a69b91a15cd65d654bf05bdf638b8ef2e3
m-proniaiaev <69893242+m-proniaiaev@users.noreply.github.com>
2026-04-02 13:43:49 +0300

    Add fetch --prune to cleanup script and alias

--- prune_deleted_remotes.sh
+++ prune_deleted_remotes.sh
@@ -1,3 +1,3 @@
-git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs -r git branch -d -f
+git fetch --prune | git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs -r git branch -d -f
 
-git config --global alias.cleanup '!git branch -vv | grep ": gone]" | grep -v "\*" | awk "{ print \$1; }" | xargs -r git branch -d -f'
+git config --global alias.cleanup '!git fetch --prune | git branch -vv | grep ": gone]" | grep -v "\*" | awk "{ print \$1; }" | xargs -r git branch -d -f'
